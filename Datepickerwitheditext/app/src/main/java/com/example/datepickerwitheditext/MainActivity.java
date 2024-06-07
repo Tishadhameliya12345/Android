1de3b1db7181ca1cdd29d4697a857f8e6eedf836
Tishadhameliya12345 <159407986+Tishadhameliya12345@users.noreply.github.com>
2024-06-07 10:23:01 +0530

    DatePicker

---
+++ Datepickerwitheditext/app/src/main/java/com/example/datepickerwitheditext/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.datepickerwitheditext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=(EditText) findViewById(R.id.edittext);
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c=Calendar.getInstance();
+                int mYear=c.get(Calendar.YEAR);
+                int mMonth=c.get(Calendar.MONTH);
+                int mDay=c.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthofyear, int dayofmonth) {
+                        editText.setText(dayofmonth+"/"+(monthofyear+1)+"/"+year);
+                    }
+                },mYear,mMonth,mDay);
+                datePickerDialog.show();
+            }
+        });
+    }
+}
